911bf7d541e1d5db67440b8d494a2217771643e6
Thomas Richter <thomas.karl.richter@gmail.com>
2022-08-26 10:57:41 +0200

    remove minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const markdownIt = require("markdown-it");
 const yaml = require("js-yaml");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
   let options = {
@@ -13,18 +12,6 @@ module.exports = function (eleventyConfig) {
     dynamicPartials: false,
     root: ["_includes", "."],
   });
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if (outputPath && outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-      });
-      return minified;
-    }
-  });
-
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("files");
   eleventyConfig.addPassthroughCopy("CNAME");
